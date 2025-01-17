1378cad066773e5452c89c094a534bd4d70e7eee
kishlay-kumar7 <kishlay141@gmail.com>
2025-01-18 00:11:27 +0530

    Update project dependencies, modify main components, and clean up unused assets
    - Updated project dependencies: Modified package.json and package-lock.json to reflect changes in the project dependencies.
    - Refactored main components: Adjusted and updated the core files, including App.jsx, main.jsx, and tailwind.config.js, to improve functionality or design.
    - Removed unused assets: Deleted src/assets/react.svg, as the asset is no longer required in the project.
    - Added untracked directories: Introduced new untracked directories for src/components/ and src/pages/ as part of upcoming updates.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SingupPage from "./pages/SignupPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
 const App = () => {
-  return <div className="text-red-600">Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/singup" element={<SingupPage />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
