4e0dc221641b0a3ea63ee3221f8c5f0b3fa652cd
kishlay-kumar7 <kishlay141@gmail.com>
2025-01-18 16:57:38 +0530

    Update backend dependencies and files

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,18 @@ const app = express();
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import { router as authRoutes } from "./routes/auth.route.js";
 import { router as messageRoutes } from "./routes/message.route.js";
 import { connectDb } from "./lib/db.js";
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const port = process.env.PORT || 6000;
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
