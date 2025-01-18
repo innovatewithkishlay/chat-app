a241c78a3d2ea9baad43a39b5abe8b0cded937e4
kishlay-kumar7 <kishlay141@gmail.com>
2025-01-18 16:54:16 +0530

    Update frontend and backend dependencies, add changes to package.json and package-lock.json
    - Modified frontend package.json and package-lock.json
    - Updated backend package.json, package-lock.json, and index.js
    - Updated App.jsx
    - Added untracked files in the backend node_modules
    
    Note: Ensure backend dependencies are installed after commit.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,46 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-import SingupPage from "./pages/SignupPage";
+import SignupPage from "./pages/SignupPage";
 import SettingPage from "./pages/SettingPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log({ authUser });
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-14 animate-spin" />
+      </div>
+    );
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/singup" element={<SingupPage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <Navigate to={"/"} />}
+        />
         <Route path="/settings" element={<SettingPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
+        />
       </Routes>
     </div>
   );
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningIn: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+  checkAuth: async () => {
+    try {
+      const user = await axiosInstance("/auth/check");
+      set({ authUser: user.data });
+    } catch (error) {
+      console.log(
+        "somehthing went wrong inside the useauthstore inside frotend folder insde the store folder ",
+        error.message
+      );
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
+export { useAuthStore };
