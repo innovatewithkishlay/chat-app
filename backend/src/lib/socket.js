fe1a8eae689bb72e71129a071e520efd65118cbb
kishlay-kumar7 <kishlay141@gmail.com>
2025-01-23 17:02:54 +0530

    changed the socket.js file to add lisning thing to get console message

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -2,11 +2,17 @@ import { Server } from "socket.io";
 import express from "express";
 import http from "http";
 import express from "express";
+import { Socket } from "dgram";
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: ["http://localhost:5173"] },
 });
-
+io.on("connection", (socket) => {
+  console.log("A connection has been established with socket io ", socket.id);
+  socket.on("disconnect", () => {
+    console.log("Disconnected", socket.id);
+  });
+});
 export { io, app, server };
