1534d77c571cd5e226053e68ce90ba7e5377b802
kishlay-kumar7 <kishlay141@gmail.com>
2025-01-24 20:22:55 +0530

    changed the login and auth to get only one error at a time like not invalid email or password must be filled only one error at a time which wasn't before

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,23 +6,21 @@ import { isValidEmailDomain } from "../lib/utils/domainValidator.js";
 export const signup = async (req, res) => {
   const { fullname, email, password } = req.body;
   try {
-    if (!isValidEmailDomain(email)) {
+    if (!isValidEmailDomain(email) || password.length < 6) {
       return res.status(400).json({
-        message:
-          "Invalid email domain. Please use a valid domain like gmail.com, yahoo.com, etc.",
+        message: !isValidEmailDomain(email)
+          ? "Invalid email."
+          : "Password must be at least 6 characters.",
       });
     }
-    if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ message: "password must be at least of 6 characters." });
-    }
+
     const user = await User.findOne({ email });
     if (user) {
       return res
         .status(400)
-        .json({ message: "user with this email already exists." });
+        .json({ message: "User with this email already exists." });
     }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
@@ -31,6 +29,7 @@ export const signup = async (req, res) => {
       hashedPassword,
       password: hashedPassword,
     });
+
     if (newUser) {
       generateToken(newUser._id, res);
       await newUser.save();
@@ -44,24 +43,35 @@ export const signup = async (req, res) => {
     } else {
       res
         .status(400)
-        .json({ message: "something went wrong while creating your account" });
+        .json({ message: "Something went wrong while creating your account." });
     }
   } catch (err) {
-    console.log("Error occured in signup controller of auth", err.message);
+    console.log("Error occurred in signup controller of auth", err.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
+    if (!isValidEmailDomain(email) || !password) {
+      return res.status(400).json({
+        message: !isValidEmailDomain(email)
+          ? "Invalid email."
+          : "Password is required.",
+      });
+    }
+
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
+
     const isCorrectPassword = await bcrypt.compare(password, user.password);
     if (!isCorrectPassword) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
+
     generateToken(user._id, res);
     res.status(200).json({
       _id: user._id,
@@ -71,12 +81,13 @@ export const login = async (req, res) => {
     });
   } catch (err) {
     console.log(
-      "something went wrong while handling the controller in login ",
+      "Something went wrong while handling the controller in login",
       err
     );
     res.status(500).json({ message: "Internal error" });
   }
 };
+
 export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
