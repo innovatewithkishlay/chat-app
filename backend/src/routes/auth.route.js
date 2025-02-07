55b0c4530cd4ecdfd1e4b4a0f4651c98dd356ed0
sneha ranjan <148381062+doWithSneha@users.noreply.github.com>
2025-02-07 22:10:08 +0530

    Update auth.route.js
    docs:improved the docs by adding the relevant comments

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,6 +7,7 @@ import {
   updateProfile,
   checkUser,
 } from "../controllers/auth.controller.js";
+// now bringing the protect route from auth middleware
 import { protectRoute } from "../middlewares/auth.middleware.js";
 router.post("/signup", signup);
 router.post("/login", login);
