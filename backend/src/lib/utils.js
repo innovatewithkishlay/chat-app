6152ca4bd8c239f77a79b7f3ea2522ace424d02b
Kishlay <kishlay141@gmail.com>
2025-12-13 20:26:08 +0530

    fix: res.cookie err

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,14 +1,18 @@
 import jwt from "jsonwebtoken";
+
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "4d",
   });
-  httpOnly: true,
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
     sameSite: "none",
-      maxAge: 4 * 24 * 60 * 60 * 1000,
-        secure: true,
+    maxAge: 4 * 24 * 60 * 60 * 1000,
+    secure: true,
   });
-return token;
+
+  return token;
 };
 
 export const canUseVideoCall = (user) => {
