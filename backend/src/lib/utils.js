bb8ee68be65456af71dceb9613c6b43debfd3e26
Kishlay <kishlay141@gmail.com>
2025-12-13 20:22:13 +0530

    fix: Unauthorised - token not provided

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -3,13 +3,12 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "4d",
   });
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    sameSite: "strict",
-    maxAge: 4 * 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV != "development",
+  httpOnly: true,
+    sameSite: "none",
+      maxAge: 4 * 24 * 60 * 60 * 1000,
+        secure: true,
   });
-  return token;
+return token;
 };
 
 export const canUseVideoCall = (user) => {
